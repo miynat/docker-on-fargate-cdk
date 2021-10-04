1b92d81d4c0f4c0b2e1b4b5fc5032ccaefa0752e
Selçuk Mıynat <selcukmiynat@gmail.com>
2021-10-04 21:20:56 +0300

    Implemented the factorial calculator, and deployed on Fargate

--- factorial_calculator/factorial_calculator_stack.py
+++ factorial_calculator/factorial_calculator_stack.py
@@ -1,15 +1,19 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+)
 
 
 class FactorialCalculatorStack(cdk.Stack):
-
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "Service",
+            memory_limit_mib=1024,
+            cpu=512,
+            desired_count=2,
+            task_image_options={"image": ecs.ContainerImage.from_asset("docker/")},
+        )
